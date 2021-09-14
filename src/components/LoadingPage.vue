f2181f7c3dd370a38d959fbe588edaa8aca9235a
Rizky Ikhwan <rizkyikhwan.ri@gmail.com>
2021-09-14 11:12:36 +0700

    add loading page and change some features

---
+++ src/components/LoadingPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="overlay">
+    <div class="spin"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 2;
+}
+
+.spin {
+  display: block;
+  position: relative;
+  width: 40px;
+  height: 40px;
+  margin: 350px auto;
+  padding: 30px;
+  border: 5px solid transparent;
+  border-radius: 50%;
+  border-top-color: #00bfa6;
+  animation: spin 1s ease infinite;
+}
+
+@media (max-width: 766px) {
+  .spin {
+    margin: 300px auto;
+  } 
+}
+
+@keyframes spin {
+  to{transform: rotateZ(360deg);}
+}
+</style>
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="pageNotFound">
+    <Navbar />
+    <div class="d-flex justify-content-center">
+      <div class="row mb-5">
+        <div class="col">
+          <img
+            src="../assets/images/notfound.png"
+            alt="not found"
+            class="img-notfound img-fluid"
+          />
+          <div class="col d-flex justify-content-center mt-3 mb-3">
+            <router-link :to="{ name: 'Home' }">
+              <button class="button">GO HOME</button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "PageNotFound",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style scoped>
+.img-notfound {
+  width: 600px;
+}
+
+.button {
+  display: inline-block;
+  border: 2px solid #00bfa6;
+  border-top-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  background-color: #fff;
+  padding: 8px 10px;
+  font-size: 18px;
+  letter-spacing: 1px;
+  color: #00bfa6;
+  font-weight: 500;
+  box-shadow: inset 0 0 0 0 #00bfa6;
+  transition: .5s ease-out;
+}
+
+.button:hover {
+  box-shadow: inset 0 -100px 0 0 #00bfa6;
+  color: #fff;
+}
+</style>

ff3fad837c3bd116fab12959e6a54a36577907fb
Rizky Ikhwan <rizkyikhwan.ri@gmail.com>
2021-10-16 11:59:22 +0700

    add vue router scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -110,7 +110,14 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 router.beforeEach((to, from ,next) => {
